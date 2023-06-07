089e9d247f44bb333749bf39ba5ae257639d9616
Ruchira Tharaka <ruchirakannangara21@gmail.com>
2023-06-07 10:45:00 +0530

    Added functions to take the time and date

--- ds18b20_with_Wifi/ds18b20_with_Wifi.ino
+++ ds18b20_with_Wifi/ds18b20_with_Wifi.ino
@@ -16,12 +16,23 @@
 //Initializing the buzzer at D3
 int buzzer = D3;
 
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+//SSID and the password of Wifi connection
 char* ssid = "Dialog 4G 517";
 char* password = "576E5Fc3";
 const char* mqtt_server = "192.168.41.113";
 
-userName="serverCO326";
-passWord="group5";
+//Username and the password of mqtt broker
+char* userName="serverCO326";
+char* passWord="group5";
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
 
 //Initializing fan
 uint8_t fan = 3;  //Rx pin (GPIO3)(green wire)
@@ -35,14 +46,45 @@ DallasTemperature sensors(&oneWire);
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+int speed=0;    //Speed will be a integer between 0 and 10
+
+void updateSpeed(void Speed){
+  //This function updates the speed of the fan by writing to the pin connected to the fan
+  analogWrite(fan,Speed*124);
+}
+
+void takeTime(){
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
+
+  int splitT = formattedDate.indexOf("T");
+  dayStamp = formattedDate.substring(0, splitT);    // Extract date
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);    // Extract time
+}
+
 void callback(char* topic, byte* payload, unsigned int length){
+  //This function will receive incoming packets and call neccessary functions 
   Serial.print("Message arrived [");
   Serial.print(topic);
   String receivedMsg = "";
   for(int i=0;i<length;i++){
     receivedMsg = receivedMsg + (char)payload[i];    
   }
+  
+  updateSpeed(speed); //update the speed  
   Serial.println();
+}
+
+void sendTemperature(long Temp){
+  //This function will send temperature to the broker (in json format)
+  snprintf(msg,200,"{\"temperature\": %f, \"date\": \"%s\", \"time\": \"%s\"}",Temp,dayStamp,timeStamp);
+  client.publish("UoP_CO_326_E18_05_Temp",msg);
 }
 
 void setup_wifi(){
@@ -77,6 +119,7 @@ void reconnect() {
     // Attempt to connect
     if (client.connect(clientId,userName,passWord)) {
       Serial.println("connected");
+      client.subscribe("UoP_CO_326_E18_GrNo_Fan");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -93,13 +136,17 @@ void setup(void)
   pinMode(buzzer,OUTPUT);  //Set D3 as output for buzzer
   pinMode(fan,OUTPUT);
   analogWrite(fan,0);
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.setDebugOutput(true);
 
   setup_wifi();
   delay(1000);
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  timeClient.setTimeOffset(3600);
 }
 
 float temp;
@@ -122,17 +169,12 @@ void loop(void)
     temp = sensors.getTempCByIndex(0);
     Serial.print(temp);
     Serial.print((char)176);//shows degrees character
-    Serial.print("C  |  ");
-    analogWrite(fan,int((temp-33.0)*256.0));
-    Serial.print(int((temp-33.0)*256.0));
+    Serial.print("C");
 
-    snprintf(msg,200,"{\"temperature\": %f, \"heart rate\": %f, \"pulse rate\": %f, \"oxygen saturation\": %f}",value1,value2,value3,value4);
-    client.publish("outtopic","Hello world");
+    sendTemperature(temp);
 
     if(temp>35){
       tone(buzzer,1000,200); 
     }
   }
 }
-
-//client.subscribe("greenBottles/#");
